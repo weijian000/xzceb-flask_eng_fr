96a45dee3b01d716be0c446d0f98c3f741cdd609
weijian000 <weijian1311@gmail.com>
2021-12-09 10:40:19 +0000

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,37 @@
+#!/usr/bin/env python3
+
+"""
+Server module
+"""
+
+import json
 from machinetranslation import translator
 from flask import Flask, render_template, request
-import json
 
-app = Flask("Web Translator")
+APP = Flask( "Web Translator" )
 
-@app.route("/englishToFrench")
-def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+@APP.route( "/englishToFrench" )
+def english_to_french():
+    """
+    End point implementation: en -> fr
+    """
+    text_to_translate = request.args.get( 'textToTranslate' )
+    return translator.english_to_french( text_to_translate )
 
-@app.route("/frenchToEnglish")
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+@APP.route( "/frenchToEnglish" )
+def french_to_english():
+    """
+    End point implementation: fr -> en
+    """
+    text_to_translate = request.args.get( 'textToTranslate' )
+    return translator.french_to_english( text_to_translate )
 
-@app.route("/")
-def renderIndexPage():
-    # Write the code to render template
+@APP.route( "/" )
+def render_index_page():
+    """
+    End point implementation: index
+    """
+    return render_template( "index.html" )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    APP.run( debug = True, host = "0.0.0.0", port = 8080 )
